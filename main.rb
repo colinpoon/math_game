dd1fe51bd4e0f8f682d33bf2a9935c2acdfde387
Colin Poon <me@colinpoon.com>
2021-11-10 10:57:45 -0800

    moved logic from player -> main

--- main.rb
+++ main.rb
@@ -2,4 +2,14 @@ require './game.rb'
 require './questions.rb'
 require './players.rb'
 
-print "hello"
+# print "hello"
+print "Player 1, choose a nickname -> \n"
+p1 = = $stdin.gets.chomp
+print "Welcome, #{p1}!\n"
+print "Player 2, choose a nickname -> \n"
+p2 = $stdin.gets.chomp
+print "Welcome, #{p2}!\n"
+
+new_game = Game.new(p1, p2)
+
+new_game.start
