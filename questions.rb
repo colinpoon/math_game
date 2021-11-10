0e699f094239b8f6fba2256c8f6cadfb657abc47
Colin Poon <me@colinpoon.com>
2021-11-09 16:08:17 -0800

    basic math question & input

--- questions.rb
+++ questions.rb
@@ -1,8 +1,37 @@
-class Question
-  def initialize
-  int_one = rand(1..20)
-  int_two = rand(1..20)
-  sum = int_two + int_two
+# class Question
+#   def initialize
+#   @int_one = rand(1..20)
+#   @int_two = rand(1..20)
+#   @sum = int_two + int_two
+#   end
+# end
+
+
+int_one = rand(1..20)
+int_two = rand(1..20)
+sum = int_two + int_two
+
+puts "whats the sum of #{int_one} + #{int_two}?"
+
+player_answer = gets.chomp.to_i
+
+  # case $player_answer
+  # when player_answer != sum
+  #    puts "WRONG! #{int_one} + #{int_two} = #{sum}"
+  # else
+  #    puts "CORRECT! #{int_one} + #{int_two} = #{sum}"
+  # end
+
+  case $sum
+  when  sum != player_answer
+     puts "WRONG! #{int_one} + #{int_two} = #{sum}"
+  else
+     puts "CORRECT! #{int_one} + #{int_two} = #{sum}"
   end
-end
-puts sum
+
+#   if player_answer == sum
+#     puts "CORRECT!"
+#   else
+#     puts "WRONG!"
+#  end
+
